37ebd45daf380334c333e161202332a358e7384e
Yeeziz <giannuzzisebastiano@gmail.com>
2022-02-16 19:19:41 +0000

    Alpha modified, Enabled INSERT in database

--- alpha/alpha.php
+++ alpha/alpha.php
@@ -5,7 +5,7 @@ $password = "test";
 
 // Create connection
 try {
-  $db = new PDO('mysql:host=db;dbname=benchmarks', 'root', 'root');
+  $db = new PDO('mysql:host=db;dbname=localhost', 'root', 'test');
 } catch (PDOException $e) {
   print "Error!: " . $e->getMessage() . "<br/>";
   die();
@@ -16,4 +16,11 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$Data = date("Y-m-d",time());
+$Apache = "alpha";
+$sql = "INSERT INTO Richieste (Data, Apache) VALUES (?,?)";
+$db->prepare($sql)->execute([$Data, $Apache]);
+
+echo "alpha";
 ?>
--- beta/beta.php
+++ beta/beta.php
@@ -16,4 +16,6 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+echo "beta";
 ?>
